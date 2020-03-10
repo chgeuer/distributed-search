5bff7d1b1ffeea6b9b231fb2d12f48f37e9ad6ec
Dr. Christian Geuer-Pollmann <chgeuer@microsoft.com>
2020-03-10 18:19:59 +0100

    Reformat

--- Domain/UpdateLogic.fs
+++ Domain/UpdateLogic.fs
@@ -7,5 +7,11 @@ module BusinessDataExtensions =
     [<Extension>]
     let Update(businessData: BusinessData, version: UpdateOffset, update: BusinessDataUpdate) =
         match update with
-        | MarkupUpdate(fashionType, markupPrice) -> { businessData with Version = version ; Markup = businessData.Markup.Add(fashionType, markupPrice) }
-        | BrandUpdate(key, value) -> { businessData with Version = version ; Brands = businessData.Brands.Add(key, value) }
+        | MarkupUpdate(fashionType, markupPrice) -> 
+            { businessData with
+                Version = version ;
+                Markup = businessData.Markup.Add(fashionType, markupPrice) }
+        | BrandUpdate(key, value) ->
+            { businessData with 
+                Version = version ;
+                Brands = businessData.Brands.Add(key, value) }
