6c5c94b334409bcf589a51fed9e40ca50ee95f87
Dr. Christian Geuer-Pollmann <chgeuer@microsoft.com>
2020-04-05 16:37:16 +0200

    .

---
+++ Fundamentals/BusinessLogic.fs
@@ -0,0 +1,25 @@
+﻿module BusinessLogic.Logic
+
+type Predicate<'context, 'item> =
+    'context * 'item -> bool
+
+type Projection<'context, 'item> =
+    'context * 'item -> 'item
+    
+type PipelineStep<'context, 'item> =
+    | Predicate of Predicate<'context, 'item>
+    | Projection of Projection<'context, 'item>
+
+type PipelineSteps2<'context, 'item> =
+    { StreamingSteps:  PipelineStep<'context, 'item> list
+      SequentialSteps: PipelineStep<'context, 'item> list }
+
+type ComparisonResult =
+    | NotComparable = 0
+    | NotBetterAlternative = -1
+    | BetterAlternative = 1
+
+type ReplaceableOption<'a> =
+    | None
+    | Some of 'a
+    | ReplaceEntry of OldItem: 'a * NewItem: 'a
