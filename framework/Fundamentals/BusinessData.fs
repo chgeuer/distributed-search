5ebb24ea611bca78ff21add825c952af66db919f
Dr. Christian Geuer-Pollmann <chgeuer@microsoft.com>
2020-04-23 18:46:42 +0200

    a

--- framework/Fundamentals/BusinessData.fs
+++ framework/Fundamentals/BusinessData.fs
@@ -1,35 +1,44 @@
-﻿module Mercury.Fundamentals.BusinessData
+﻿namespace Mercury.Fundamentals
 
-open System
-open Mercury.Fundamentals.Types
+open System.Runtime.CompilerServices
 
-type UpdateOutOfOrderError =
-    { DataOffset: Offset 
-      UpdateOffset: Offset }
+[<Extension>]
+module BusinessData =
 
-type BusinessDataUpdateError = 
-    | UpdateOutOfOrderError of UpdateOutOfOrderError
-    | SnapshotDownloadError of Exception
+    open System
+    open Mercury.Fundamentals.Types
 
-type BusinessData<'domainSpecificBusinessData> =
-    { Data: 'domainSpecificBusinessData
-      Offset: Offset }
+    type UpdateOutOfOrderError =
+        { DataOffset: Offset 
+          UpdateOffset: Offset }
 
-type SingleUpdate<'domainSpecificBusinessData, 'domainSpecificUpdate> =
-    'domainSpecificBusinessData -> 'domainSpecificUpdate -> 'domainSpecificBusinessData
+    type BusinessDataUpdateError = 
+        | UpdateOutOfOrderError of UpdateOutOfOrderError
+        | SnapshotDownloadError of Exception
 
-let updateBusinessData<'domainSpecificBusinessData, 'domainSpecificUpdate> (func: SingleUpdate<'domainSpecificBusinessData, 'domainSpecificUpdate>) (data: Result<BusinessData<'domainSpecificBusinessData>, BusinessDataUpdateError>) (um: Message<'domainSpecificUpdate>): Result<BusinessData<'domainSpecificBusinessData>, BusinessDataUpdateError> =
-    match data with
-    | Error e -> Error e
-    | Ok data ->
-        let newVersion = 
-            match (data.Offset, um.Offset) with
-            | (dataOffset, updateOffset) when dataOffset.Add(1L) <> updateOffset -> 
-                Error { DataOffset = dataOffset; UpdateOffset = updateOffset }
-            | (_, updateOffset) -> Ok updateOffset
+    type BusinessData<'domainSpecificBusinessData> =
+        { Data: 'domainSpecificBusinessData
+          Offset: Offset }
 
-        match newVersion with
-            | Error e -> Error (UpdateOutOfOrderError e)
-            | Ok newVersion -> 
-                let newData = func data.Data um.Payload
-                Ok { data with Offset = newVersion; Data = newData }
+    type SingleUpdate<'domainSpecificBusinessData, 'domainSpecificUpdate> =
+        'domainSpecificBusinessData -> 'domainSpecificUpdate -> 'domainSpecificBusinessData
+
+    let updateBusinessData<'domainSpecificBusinessData, 'domainSpecificUpdate> (func: SingleUpdate<'domainSpecificBusinessData, 'domainSpecificUpdate>) (data: Result<BusinessData<'domainSpecificBusinessData>, BusinessDataUpdateError>) (um: Message<'domainSpecificUpdate>): Result<BusinessData<'domainSpecificBusinessData>, BusinessDataUpdateError> =
+        match data with
+        | Error e -> Error e
+        | Ok data ->
+            let newVersion = 
+                match (data.Offset, um.Offset) with
+                | (dataOffset, updateOffset) when dataOffset.Add(1L) <> updateOffset -> 
+                    Error { DataOffset = dataOffset; UpdateOffset = updateOffset }
+                | (_, updateOffset) -> Ok updateOffset
+
+            match newVersion with
+                | Error e -> Error (UpdateOutOfOrderError e)
+                | Ok newVersion -> 
+                    let newData = func data.Data um.Payload
+                    Ok { data with Offset = newVersion; Data = newData }
+
+    //[<Extension>]
+    //let ApplyUpdates (data: UpdateableData) (updates: Update list): UpdateableData =
+    //    updates |> List.fold ApplyUpdate data
